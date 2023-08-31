4b8acc5cf1a84fe7076141217fd7df00d6cb031a
juanravm <juanrafaelvalera00@gmail.com>
2023-08-31 14:32:40 +0200

    Update

--- code/picrust2_plot.R
+++ code/picrust2_plot.R
@@ -72,12 +72,14 @@ metadata <- as.data.frame(read_delim(metadata_fp,
 
 colnames(metadata)[1] <- "sample_name"
 
+metadata <- metadata[!is.na(metadata[,col]),]
+
 if (method=="KO"){
   kegg_abundance <- ko2kegg_abundance(KO)
   kegg_abundance <- kegg_abundance[, metadata$sample_name]
   
-group <- col
-kegg_daa_df <-
+  group <- col
+  kegg_daa_df <-
     pathway_daa(
       abundance = kegg_abundance,
       metadata = metadata,
@@ -88,147 +90,204 @@ kegg_daa_df <-
       reference = ref_group
     )
   
-kegg_daa_sub_method_results_df <-
-    kegg_daa_df[kegg_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
+  if (length(unique(metadata[,col]))==2) {
+    kegg_daa_sub_method_results_df <-
+      kegg_daa_df[kegg_daa_df$method == "ALDEx2_Wilcoxon rank test", ]
+  } else if (length(unique(metadata[,col]))>2) {
+    kegg_daa_sub_method_results_df <-
+      kegg_daa_df[kegg_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
+  }
   
-kegg_daa_annotated_sub_method_results_df <-
+  kegg_daa_annotated_sub_method_results_df <-
     pathway_annotation(pathway = "KO",
                        daa_results_df = kegg_daa_sub_method_results_df,
                        ko_to_kegg = TRUE)
-
-write.table(kegg_daa_annotated_sub_method_results_df, 
-              file = paste(Visualizations, "/KO.tsv"), 
+  
+  write.table(kegg_daa_annotated_sub_method_results_df, 
+              file = paste(Visualizations, "/KO.tsv", sep = ""), 
               sep = "\t", 
               quote = FALSE, 
               row.names = F, 
               col.names = T)
   
-Group <- Group <-metadata [,col]
+  a <- pathway_pca(abundance = kegg_abundance,
+              metadata = tibble(metadata),
+              group = col)
   
-# Removing NA in pathways name and rounding p_adjust to 5 decimals
-kegg_daa_annotated_sub_method_results_df <- kegg_daa_annotated_sub_method_results_df[!is.na(kegg_daa_annotated_sub_method_results_df$pathway_name),]
-kegg_daa_annotated_sub_method_results_df$p_adjust <- round(kegg_daa_annotated_sub_method_results_df$p_adjust,5)
+  ggsave("KO_PCA.png",
+         path = Visualizations,
+         plot = a, 
+         dpi = 320)
   
-# Selecting the 20 more significant pathways in the errorbar for plotting
-low_p_feature <- kegg_daa_annotated_sub_method_results_df[order(kegg_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
+  Group <- Group <-metadata [,col]
   
-p <- pathway_errorbar(abundance = kegg_abundance,
-                   daa_results_df = kegg_daa_annotated_sub_method_results_df,
+  # Removing NA in pathways name and rounding p_adjust to 5 decimals
+  kegg_daa_annotated_sub_method_results_df <- kegg_daa_annotated_sub_method_results_df[!is.na(kegg_daa_annotated_sub_method_results_df$pathway_name),]
+  kegg_daa_annotated_sub_method_results_df$p_adjust <- round(kegg_daa_annotated_sub_method_results_df$p_adjust,5)
+  
+  # Selecting the 20 more significant pathways in the errorbar for plotting
+  low_p_feature <- kegg_daa_annotated_sub_method_results_df[order(kegg_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
+  
+  p <- pathway_errorbar(abundance = kegg_abundance,
+                        daa_results_df = kegg_daa_annotated_sub_method_results_df,
+                        Group = Group,
+                        ko_to_kegg = TRUE,
+                        p_values_threshold = 0.05,
+                        order = "pathway_class",
+                        select = low_p_feature,
+                        p_value_bar = TRUE,
+                        colors = NULL,
+                        x_lab = "pathway_name")
+  
+  ggsave("KO.png",
+         path = Visualizations,
+         plot = p, 
+         dpi = 320)
+  
+} else if (method=="MetaCyc"){
+  ## Data import
+  MetaCyc_abundance <- read.table(MetaCyc, sep = "\t", header = F, row.names = NULL)
+  rownames(MetaCyc_abundance) <- MetaCyc_abundance[,1]
+  colnames(MetaCyc_abundance) <- MetaCyc_abundance[1,]
+  MetaCyc_abundance <- MetaCyc_abundance[-1,-1]
+  
+  MetaCyc_abundance <- MetaCyc_abundance[, metadata$sample_name]
+  
+  for (i in 1:ncol(MetaCyc_abundance)) {
+    MetaCyc_abundance[,i] <- as.numeric(MetaCyc_abundance[,i])
+  }
+  
+  group <- col
+  Metacyc_daa_df <-
+    pathway_daa(
+      abundance = MetaCyc_abundance,
+      metadata = metadata,
+      group = group,
+      daa_method = "ALDEx2",
+      select = NULL,
+      p.adjust = "BH",
+      reference = ref_group
+    )
+  
+  if (length(unique(metadata[,col]))==2) {
+    Metacyc_daa_sub_method_results_df <-
+      Metacyc_daa_df[Metacyc_daa_df$method == "ALDEx2_Wilcoxon rank test", ]
+  } else if (length(unique(metadata[,col]))>2) {
+  Metacyc_daa_sub_method_results_df <-
+    Metacyc_daa_df[Metacyc_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
+  }
+  
+  Metacyc_daa_annotated_sub_method_results_df <-
+    pathway_annotation(pathway = "MetaCyc",
+                       daa_results_df = Metacyc_daa_sub_method_results_df,
+                       ko_to_kegg = F)
+  
+  write.table(Metacyc_daa_annotated_sub_method_results_df, 
+              file = paste(Visualizations, "/MetaCyc.tsv", sep = ""), 
+              sep = "\t", 
+              quote = FALSE, 
+              row.names = F, 
+              col.names = T)
+  
+  a <- pathway_pca(abundance = MetaCyc_abundance,
+                   metadata = tibble(metadata),
+                   group = col)
+  
+  ggsave("MetaCyc_PCA.png",
+         path = Visualizations,
+         plot = a, 
+         dpi = 320)
+  
+  Group <-metadata[,col]
+  
+  Metacyc_daa_annotated_sub_method_results_df$p_adjust <- round(Metacyc_daa_annotated_sub_method_results_df$p_adjust,5)
+  
+  low_p_feature <- Metacyc_daa_annotated_sub_method_results_df[order(Metacyc_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
+  
+  p <- pathway_errorbar(abundance = MetaCyc_abundance,
+                        daa_results_df = Metacyc_daa_annotated_sub_method_results_df,
+                        Group = Group,
+                        ko_to_kegg = F,
+                        p_values_threshold = 0.05,
+                        order = "group",
+                        select = low_p_feature,
+                        p_value_bar = TRUE,
+                        colors = NULL,
+                        x_lab = "description")
+  
+  ggsave("MetaCyc.png",
+         path = Visualizations,
+         plot = p, 
+         dpi = 320)
+  
+} else if (method=="EC"){
+  EC_abundance <-
+    read.table(EC, sep = "\t", header = F, row.names = NULL)
+  rownames(EC_abundance) <- EC_abundance[,1]
+  colnames(EC_abundance) <- EC_abundance[1,]
+  EC_abundance <- EC_abundance[-1,-1]
+  
+  EC_abundance <- EC_abundance[, metadata$sample_name]
+  
+  for (i in 1:ncol(EC_abundance)) {
+    EC_abundance[,i] <- as.numeric(EC_abundance[,i])
+  }
+  
+  group <- col
+  EC_daa_df <-
+    pathway_daa(
+      abundance = EC_abundance,
+      metadata = metadata,
+      group = group,
+      daa_method = "ALDEx2",
+      select = NULL,
+      p.adjust = "BH",
+      reference = ref_group
+    )
+  
+  if (length(unique(metadata[,col]))==2) {
+    EC_daa_sub_method_results_df <-
+      EC_daa_df[EC_daa_df$method == "ALDEx2_Wilcoxon rank test", ]
+  } else if (length(unique(metadata[,col]))>2) {
+    EC_daa_sub_method_results_df <-
+      EC_daa_df[EC_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
+  }
+  
+  EC_daa_annotated_sub_method_results_df <-
+    pathway_annotation(pathway = "EC",
+                       daa_results_df = EC_daa_sub_method_results_df,
+                       ko_to_kegg = F)
+  
+  write.table(EC_daa_annotated_sub_method_results_df, 
+              file = paste(Visualizations, "/EC.tsv", sep = ""), 
+              sep = "\t", 
+              quote = FALSE, 
+              row.names = F, 
+              col.names = T)
+  
+  a <- pathway_pca(abundance = EC_abundance,
+                   metadata = tibble(metadata),
+                   group = col)
+  
+  ggsave("EC_PCA.png",
+         path = Visualizations,
+         plot = a, 
+         dpi = 320)
+  
+  EC_daa_annotated_sub_method_results_df$p_adjust <- round(EC_daa_annotated_sub_method_results_df$p_adjust,5)
+  
+  # Selecting the 20 more significant pathways in the errorbar for plotting
+  low_p_feature <- EC_daa_annotated_sub_method_results_df[order(EC_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
+  
+  Group <-metadata[,col]
+  pathway_errorbar(abundance = EC_abundance_daa,
+                   daa_results_df = EC_daa_annotated_sub_method_results_df,
                    Group = Group,
-                   ko_to_kegg = TRUE,
+                   ko_to_kegg = F,
                    p_values_threshold = 0.05,
-                   order = "pathway_class",
+                   order = "group",
                    select = low_p_feature,
                    p_value_bar = TRUE,
                    colors = NULL,
-                   x_lab = "pathway_name")
-                   
-ggsave("KO.png",
-       path = Visualizations,
-       plot = p, 
-       dpi = 320)
-       
-} else if (method=="MetaCyc"){
-## Data import
-MetaCyc_abundance <- read.table(MetaCyc, sep = "\t", header = F, row.names = NULL)
-rownames(MetaCyc_abundance) <- MetaCyc_abundance[,1]
-colnames(MetaCyc_abundance) <- MetaCyc_abundance[1,]
-MetaCyc_abundance <- MetaCyc_abundance[-1,-1]
-
-MetaCyc_abundance <- MetaCyc_abundance[, metadata$sample_name]
-
-group <- col
-Metacyc_daa_df <-
-  pathway_daa(
-    abundance = MetaCyc_abundance,
-    metadata = metadata,
-    group = group,
-    daa_method = "ALDEx2",
-    select = NULL,
-    p.adjust = "BH",
-    reference = ref_group
-  )
-
-Metacyc_daa_sub_method_results_df <-
-  Metacyc_daa_df[Metacyc_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
-  
-Metacyc_daa_annotated_sub_method_results_df <-
-  pathway_annotation(pathway = "MetaCyc",
-                     daa_results_df = Metacyc_daa_sub_method_results_df,
-                     ko_to_kegg = F)
-                     
-write.table(Metacyc_daa_annotated_sub_method_results_df, 
-              file = paste(Visualizations, "/MetaCyc.tsv"), 
-              sep = "\t", 
-              quote = FALSE, 
-              row.names = F, 
-              col.names = T)
-                     
-Group <-metadata[,col]
-
-Metacyc_daa_annotated_sub_method_results_df$p_adjust <- round(Metacyc_daa_annotated_sub_method_results_df$p_adjust,5)
-
-low_p_feature <- Metacyc_daa_annotated_sub_method_results_df[order(Metacyc_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
-
-p <- pathway_errorbar(abundance = MetaCyc_abundance,
-                 daa_results_df = Metacyc_daa_annotated_sub_method_results_df,
-                 Group = Group,
-                 ko_to_kegg = F,
-                 p_values_threshold = 0.05,
-                 order = "group",
-                 select = low_p_feature,
-                 p_value_bar = TRUE,
-                 colors = NULL,
-                 x_lab = "description")
-                 
-ggsave("MetaCyc.png",
-       path = Visualizations,
-       plot = p, 
-       dpi = 320)
-       
-} else if (method=="EC"){
-EC_abundance <-
-  read.table(EC, sep = "\t", header = F, row.names = NULL)
-rownames(EC_abundance) <- EC_abundance[,1]
-colnames(EC_abundance) <- EC_abundance[1,]
-EC_abundance <- EC_abundance[-1,-1]
-
-EC_abundance <- EC_abundance[, metadata$sample_name]
-
-group <- col
-EC_daa_df <-
-  pathway_daa(
-    abundance = EC_abundance,
-    metadata = metadata,
-    group = group,
-    daa_method = "ALDEx2",
-    select = NULL,
-    p.adjust = "BH",
-    reference = ref_group
-  )
-
-EC_daa_sub_method_results_df <-
-  EC_daa_df[EC_daa_df$method == "ALDEx2_Kruskal-Wallace test", ]
-
-EC_daa_annotated_sub_method_results_df <-
-  pathway_annotation(pathway = "EC",
-                     daa_results_df = EC_daa_sub_method_results_df,
-                     ko_to_kegg = F)
-
-EC_daa_annotated_sub_method_results_df$p_adjust <- round(EC_daa_annotated_sub_method_results_df$p_adjust,5)
-
-# Selecting the 20 more significant pathways in the errorbar for plotting
-low_p_feature <- EC_daa_annotated_sub_method_results_df[order(EC_daa_annotated_sub_method_results_df$p_adjust), ]$feature[1:20]
-
-Group <-metadata[,col]
-pathway_errorbar(abundance = EC_abundance_daa,
-                 daa_results_df = EC_daa_annotated_sub_method_results_df,
-                 Group = Group,
-                 ko_to_kegg = F,
-                 p_values_threshold = 0.05,
-                 order = "group",
-                 select = low_p_feature,
-                 p_value_bar = TRUE,
-                 colors = NULL,
-                 x_lab = "description")
+                   x_lab = "description")
 }
